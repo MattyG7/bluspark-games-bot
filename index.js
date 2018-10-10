7c9ceffefbfa6fedbf66bf3077a30d800b66315f
MattyG7 <10830691+MattyG7@users.noreply.github.com>
2018-10-11 00:05:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
@@ -884,4 +883,4 @@ bot.on("message", async message => {
 
 
 });
-bot.login(botconfig.token);
+bot.login(process.env.BOT_TOKEN);
