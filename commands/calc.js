0483b28cb53d70f5eeeb1907852d6f7cf9e16a31
MattyG7 <10830691+MattyG7@users.noreply.github.com>
2018-10-16 18:34:50 +0100

    Update calc.js

--- commands/calc.js
+++ commands/calc.js
@@ -26,7 +26,7 @@ module.exports.run = async (bot, message, args) => {
     let num1 = args[0];
     let num2 = args[2];
     let sum = `${args[0]} + ${args[2]}`;
-    let ans = args[0] +args[2];
+    let ans = Number(args[0]) + Number(args[2]);
 
     let mthembed = new Discord.RichEmbed()
     .setColor("#1fd1c8")
