16e4c36b67a53d4351b281cd88463782eedcd2c1
MattyG7 <10830691+MattyG7@users.noreply.github.com>
2018-10-17 22:13:55 +0100

    Update laugh.js

--- commands/laugh.js
+++ commands/laugh.js
@@ -5,13 +5,37 @@ module.exports.run = async (bot, message, args) => {
   if(!rUser) return message.channel.send(`User not found! 😕`);
   if(rUser.id === message.author.id) return message.channel.send(`***${message.author.username} is laughing at themself. Everyone, back away slowly...***`);
 
-  let tngicon = message.author.displayAvatarURL;
-  let tngembed = new Discord.RichEmbed()
+  if (!args[0]) {
+    let replies = ["0", "1"];
+    let result = Math.floor((Math.random() * replies.length));
+
+    if(result === 0) {
+      let lghicon = message.author.displayAvatarURL;
+      let lghicon = new Discord.RichEmbed()
+      .setColor("#1fd1c8")
+      //.setAuthor("Laugh 😂", tngicon)
+      .setAuthor(`***${message.author.username} finds that funny!`, lghicon)
+      .setImage (`https://media1.tenor.com/images/f7d0b534e95c24a53b9767b480e76df3/tenor.gif?itemid=11203476`);
+      return message.channel.send(lghembed);
+    }
+    if(result === 1) {
+      let lghicon = message.author.displayAvatarURL;
+      let lghicon = new Discord.RichEmbed()
+      .setColor("#1fd1c8")
+      //.setAuthor("Laugh 😂", tngicon)
+      .setAuthor(`***${message.author.username} finds that funny!`, lghicon)
+      .setImage (`https://media1.tenor.com/images/2dcfc6694bb1a931c60bb67a6323e40d/tenor.gif?itemid=10067925`);
+      return message.channel.send(lghembed);
+    }
+  }
+
+  let lghicon = message.author.displayAvatarURL;
+  let lghembed = new Discord.RichEmbed()
   .setColor("#1fd1c8")
-  .setAuthor("Laugh 😂", tngicon)
+  .setAuthor("Laugh 😂", lghicon)
   .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
   .setImage (`https://media1.tenor.com/images/b3e0ecd965e324aa328a0137c38a44f1/tenor.gif`);
-  return message.channel.send(tngembed);
+  return message.channel.send(lghembed);
 }
 
 module.exports.help = {
