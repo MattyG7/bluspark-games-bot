452033ca3f352a44392ed54a46156bb938b3a704
MattyG7 <10830691+MattyG7@users.noreply.github.com>
2018-10-18 14:12:16 +0100

    Update blumoji.js

--- commands/blumoji.js
+++ commands/blumoji.js
@@ -5,10 +5,12 @@ module.exports.run = async (bot, message, args) => {
   let result = Math.floor((Math.random() * replies.length));
 
   if(result === 0) {
-    return message.channel.send(`\:spark:`);
+    const spark = client.emojis.find(emoji => emoji.name === "spark");
+    return message.channel.send(spark);
   }
   if(result === 1) {
-    return message.channel.send(\:masterball:);
+    const masterball = client.emojis.find(emoji => emoji.name === "masterball");
+    return message.channel.send(masterball);
   }
 }
 
