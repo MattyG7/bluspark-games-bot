67a9fd815d53448595e2837db8be64adb5514e8c
MattyG7 <10830691+MattyG7@users.noreply.github.com>
2018-10-18 14:47:31 +0100

    Update helpx.js

--- commands/helpx.js
+++ commands/helpx.js
@@ -1,24 +1,22 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  let helpembed = new Discord.RichEmbed()
-  .setDescription("**Commands:**")
+  let helpxembed = new Discord.RichEmbed()
+  .setDescription("**Command Examples:**")
   .setColor("#1fd1c8")
-  .addField("Profile Stuff", "`profile, wallet, level, daily, setup`", )
-  .addField("Greets", "`hello, hi, hey, goodbye, bye, goodnight, night`")
-  .addField("Other Phrases", "`ilikeyou, iloveyou, ihateyou`")
-  .addField("Actions", "`poke, punch, kill, greet, slap, lick, pinch, pat, hug, kiss, snog, bite, eat, stickouttongue, sad, cry, laugh, congrats, scare, dance, brushhair`")
-  .addField("Emotions & Solo Actions", "`sad, cry, laugh, dance, brushhair`")
-  .addField("Games", "`flip, roll, bowl, penalty, rps, memory, deal, dontgetbit`")
-  .addField("Fun", "`8ball, flip, roll, bowl, penalty, say`")
-  .addField("Randomising", "`sing, colour, emoji, anime, fortune`")
-  .addField("Education", "`calc`")
-  .addField("Admin Only", "`clear, steal, spam`")
-  .addBlankField(true)
-  .addField("***For examples***", "*type ~helpx*");
-  return message.channel.send(helpembed);
+  .addField("Profile Stuff", "~profile")
+  .addField("Greets", "~hello")
+  .addField("Other Phrases", "~ilikeyou")
+  .addField("Actions", "~hug @*INSERTNAMEHERE*")
+  .addField("Emotions & Solo Actions", "`~cute`")
+  .addField("Games", "~flip heads 20 | ~roll 6 20 (please note that the order is Dice Number and then Money Amount) | ~bowl 20 | ~penalty 20 | ~rps | ~memory, ~memory e | ~deal | ~dontgetbit")
+  .addField("Fun", "~8ball is grass green | ~flip | ~roll | ~bowl | ~penalty | ~say words go here")
+  .addField("Randomising", "~sing")
+  .addField("Education", "~calc, ~calc help, ~calc *NUMBER* + *NUMBER*")
+  .addField("Admin Only", "~clear 2 | ~steal @*INSERTNAMEHERE* 100 | ~spam 2");
+  return message.channel.send(helpxembed);
 }
 
 module.exports.help = {
-  name: "help"
+  name: "helpx"
 }
