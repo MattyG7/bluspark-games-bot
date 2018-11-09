8e43c965bf411cfb5937b270a36317f4d879fc53
MattyG7 <10830691+MattyG7@users.noreply.github.com>
2018-11-09 21:18:29 +0000

    Add files via upload

---
+++ commands/cute.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  if (!args[0]) {
+    let replies = ["0", "1"];
+    let result = Math.floor((Math.random() * replies.length));
+
+    if(result === 0) {
+      let thnkicon = message.author.displayAvatarURL;
+      let thnkembed = new Discord.RichEmbed()
+      .setColor("#1fd1c8")
+      .setAuthor(`${message.author.username} is feeling cute ☺️`, thnkicon)
+      .setImage (`https://tenor.com/es/ver/charlotte-ayumi-otosaka-ayumi-otosaka-gif-5894731`);
+      return message.channel.send(thnkembed);
+    }
+    if(result === 1) {
+      let thnkicon = message.author.displayAvatarURL;
+      let thnkembed = new Discord.RichEmbed()
+      .setColor("#1fd1c8")
+      .setAuthor(`${message.author.username} is feeling cute ☺️`, thnkicon)
+      .setImage (`https://tenor.com/es/ver/charlotte-ayumi-otosaka-ayumi-otosaka-gif-5894731`);
+      return message.channel.send(thnkembed);
+    }
+  }
+  let rUser = message.mentions.members.first();
+  if(args[0] && !rUser) return message.channel.send(`User not found! 😕`);
+  if(rUser.id === message.author.id) return message.channel.send(`***${message.author.username} thinks that they're cute. Well...I don't disagree!***`);
+
+  let thnkicon = message.author.displayAvatarURL;
+  let thnkembed = new Discord.RichEmbed()
+  .setColor("#1fd1c8")
+  .setAuthor("Thinking 🤔", thnkicon)
+  .setDescription(`***${message.author.username} thinks ${rUser.user.username} is cute.***\nI can see why 😍`)
+  .setImage (`http://68.media.tumblr.com/53ca3d877092b34ca450c6722ab29a20/tumblr_ovcydrVAcl1qa94xto1_540.gif`);
+  return message.channel.send(thnkembed);
+}
+
+module.exports.help = {
+  name: "cute"
+}
